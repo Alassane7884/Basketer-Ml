196857da1367c7b1b84d36c04d13c6ef7e85316f
Alassane7884 <156536003+Alassane7884@users.noreply.github.com>
2024-01-13 18:53:46 +0000

    Add files via upload

---
+++ coachprivéescript.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth1').lightSlider({
+        autoWidth1:true,
+        onSliderLoad: function() {
+            $('#autoWidth1').removeClass('cS-hidden1');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth2:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden2');
+        }
+    })
+})
---
+++ sportshopscript.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth1').lightSlider({
+        autoWidth1:true,
+        onSliderLoad: function() {
+            $('#autoWidth1').removeClass('cS-hidden1');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth2:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden2');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth3').lightSlider({
+        autoWidth3:true,
+        onSliderLoad: function() {
+            $('#autoWidth3').removeClass('cS-hidden3');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth4').lightSlider({
+        autoWidth4:true,
+        onSliderLoad: function() {
+            $('#autoWidth4').removeClass('cS-hidden4');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth5').lightSlider({
+        autoWidth5:true,
+        onSliderLoad: function() {
+            $('#autoWidth5').removeClass('cS-hidden5');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth6').lightSlider({
+        autoWidth6:true,
+        onSliderLoad: function() {
+            $('#autoWidth6').removeClass('cS-hidden6');
+        }
+    })
+})
+
+$(document).ready(function() {
+    $('#autoWidth7').lightSlider({
+        autoWidth7:true,
+        onSliderLoad: function() {
+            $('#autoWidth7').removeClass('cS-hidden7');
+        }
+    })
+})
